e60c20b1ac512cedbc8011caf967abb8480a7c2c
Leah-nurselink <haroldtakunda@gmail.com>
2026-09-24 21:46:03 +0100

    Update MainActivity.java

--- android/app/src/main/java/app/lovable/residentvoicelog/MainActivity.java
+++ android/app/src/main/java/app/lovable/residentvoicelog/MainActivity.java
@@ -1,5 +1,13 @@
 package app.lovable.residentvoicelog;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.carecore.blediscovery.CareCoreBlePlugin;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    registerPlugin(CareCoreBlePlugin.class);
+    super.onCreate(savedInstanceState);
+  }
+}
